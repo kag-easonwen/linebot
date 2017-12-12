86afaa1b2071ade44ed7605cd46ede801b18fdf0
kag-easonwen <eason@kingasiagroup.com>
2017-12-12 20:59:51 +0800

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express');
 
 var bot = linebot({
   channelId: 1550553289,
-  channelSecret: c4c1c39fee8a0f06715c34e9c8af325f,
-  channelAccessToken: PQ04n0HIwimNU7yrcYwRS1kUNtt6NG2chZTxnv/5s3P72g2Kkd7Y9ezi5D8bKhYufH7YHCnlff7+4cSVZwxxX1MswIGU0WwJDnfyOrw/5pC1SOqbkz4bQE7l7j9f4cSeKi6i8GfL3xPcBwkKc1K0cgdB04t89/1O/w1cDnyilFU=
+  channelSecret: 'c4c1c39fee8a0f06715c34e9c8af325f',
+  channelAccessToken: 'PQ04n0HIwimNU7yrcYwRS1kUNtt6NG2chZTxnv/5s3P72g2Kkd7Y9ezi5D8bKhYufH7YHCnlff7+4cSVZwxxX1MswIGU0WwJDnfyOrw/5pC1SOqbkz4bQE7l7j9f4cSeKi6i8GfL3xPcBwkKc1K0cgdB04t89/1O/w1cDnyilFU='
 });
 
 bot.on('message', function(event) {
